68ce7932724803e91829d3deb81ed242cc13c194
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2021-11-02 08:42:58 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,28 @@
+import processing.core.PApplet;
 
+public class Main extends PApplet {
+	
+	private int x = 30;
+	
+	// The argument passed to main must match the class name
+	public static void main(String[] args) {
+		PApplet.main("Main");
+	}
+		
+	// method used only for setting the size of the window
+	public void settings(){
+		size(500, 300);
+	}
+		
+	// identical use to setup in Processing IDE except for size()
+	public void setup(){
+		circle(x, height/2, 30);
+	}
+		
+	// identical use to draw in Pr0cessing IDE
+	public void draw(){
+		background(200);
+		circle(x, height/2, 30);
+		x++;
+	}
+}
